fa2dbe922f9fe7e23c0098523e0910d678b406bb
Shiplo Rahman <shiplo.msi2015@gmail.com>
2022-02-04 02:27:27 +0600

    Update main.js

--- fetch-api/main.js
+++ fetch-api/main.js
@@ -4,5 +4,8 @@ const data = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&t
 fetch(data).then(res => {
     return res;
 }).then(choice => {
-    console.log(choice);
+    console.log(choice.results);
+    choice.results.map(result => {
+        console.log(result.question);
+    });
 });
