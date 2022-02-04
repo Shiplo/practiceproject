30fc079f6868bf7698f977e860d55bb104da7620
Shiplo Rahman <shiplo.msi2015@gmail.com>
2022-02-05 03:49:51 +0600

    Update 0.5

--- quick-quiz/main.js
+++ quick-quiz/main.js
@@ -1,5 +1,5 @@
 const data = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple';
-const quizData = [
+/*const quizData = [
     {
         question: "What is the capital of Bangladesh?",
         answer1: "Chittagong",
@@ -17,56 +17,80 @@ const quizData = [
     },{
         question: "What is the meaning on HTML?",
         answer1: "Human Text Mold Lead",
-        answer2: "Myper-text Markup Language",
+        answer2: "Hyper-text Markup Language",
         answer3: "Hybrid Markup Language",
         answer4: "Language of browser",
         correct_answer: "answer2",
     }
-];
+];*/
 
-const currentQuiz = 0, quizScore = 0;
-const quizList = [...quizData];
+// Variables
+let currentQuiz = 0, quizScore = 0;
+var quizData;
+
+// Fetch Data
+fetch(data).then(res => {
+    return res.json();
+}).then(choice => {
+    //console.log(choice.results);
+    quizData = choice.results;
+    loadQuiz(quizData);
+}).catch(error => {
+    console.log(error);
+});
+
+
+// Variables
 const quizTitle = document.getElementById('quiz-title');
 const choiceLabel1 = document.getElementById('label1');
 const choiceLabel2 = document.getElementById('label2');
 const choiceLabel3 = document.getElementById('label3');
 const choiceLabel4 = document.getElementById('label4');
-
-loadQuiz();
-
-/*/ Fetch Data
-fetch(data).then(res => {
-    return res.json();
-}).then(choice => {
-    //console.log(choice.results);
-    choice.results.map(result => {
-        //console.log(result);
-        quizData = result;
-        const quizTitle = document.getElementById('quiz-title');
-        const title = document.createElement('p');
-        const wrap = document.createElement('div');
-        const label = document.createElement('label');
-        const text = document.createTextNode(result.question);
-        const correctAns = document.createTextNode(result.correct_answer);
-        title.appendChild(text);
-        label.appendChild(correctAns);
-        wrap.appendChild(title);
-        wrap.appendChild(label);
-        //quizTitle.appendChild(text);
-
-    });
-}).catch(error => {
-    console.log(error);
-});*/
+const submitbtn = document.getElementById('submit');
+const radioInputs = document.getElementsByClassName('quiz-input');
+let quizNew = {};
 
 
-function loadQuiz() {
-    const quizNew = quizList[currentQuiz];
-    console.log(quizNew.correct_answer);
-
-    quizTitle.innerText = quizNew.question;
-    choiceLabel1.innerText = quizNew.answer1;
-    choiceLabel2.innerText = quizNew.answer2;
-    choiceLabel3.innerText = quizNew.answer3;
-    choiceLabel4.innerText = quizNew.answer4;
+// View Function
+function loadQuiz(quizList) {
+    quizNew = quizList[currentQuiz];
+    //console.log(quizList);
+    if(currentQuiz < quizList.length) {
+        const randNum = Math.floor(Math.random()*4);
+        console.log(randNum);
+        console.log(quizNew.correct_answer);
+    
+        quizTitle.innerText = quizNew.question;
+        choiceLabel1.innerText = quizNew.answer1;
+        choiceLabel2.innerText = quizNew.answer2;
+        choiceLabel3.innerText = quizNew.answer3;
+        choiceLabel4.innerText = quizNew.answer4;
+    } else {
+        alert('You have finished the Quiz. Your score is '+ quizScore);
+        //location.reload();
+    }
 }
+
+// Next Quiz
+submitbtn.addEventListener('click', function() {
+    let selected = 0;
+    for(let i = 0; i < radioInputs.length; i++) {
+        if(radioInputs[i].checked) {
+            selected = 1;
+            console.log(radioInputs[i].value);
+            console.log(quizNew.correct_answer);
+            if (radioInputs[i].value == quizNew.correct_answer) {
+                quizScore = quizScore+10;
+            }
+            radioInputs[i].checked = false;
+        }
+    }
+    
+    // Check if select on choice then load next question
+    if(selected === 1) {
+        currentQuiz++;
+        loadQuiz();
+    } else {
+        alert('Select one answer first.')
+    }
+});
